19920b75a42d0390cd92fd4b6a0a2033c7d82112
LukasGP1 <lukas.hilger@icloud.com>
2023-12-03 09:03:11 +0100

    set up the class for custom items

--- src/main/java/de/lulkas_/testmod/TestMod.java
+++ src/main/java/de/lulkas_/testmod/TestMod.java
@@ -1,6 +1,7 @@
 package de.lulkas_.testmod;
 
 import com.mojang.logging.LogUtils;
+import de.lulkas_.testmod.item.ModItems;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -23,6 +24,8 @@ public class TestMod {
 
     public TestMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
---
+++ src/main/java/de/lulkas_/testmod/item/ModItems.java
@@ -0,0 +1,16 @@
+package de.lulkas_.testmod.item;
+
+import de.lulkas_.testmod.TestMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MOD_ID);
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
